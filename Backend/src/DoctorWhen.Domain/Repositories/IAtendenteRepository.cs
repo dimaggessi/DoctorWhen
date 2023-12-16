effe826cafa3119f55177f6bd300d05a9d21a56e
Diego Maggessi <diegosmteixeira@gmail.com>
2023-12-16 18:27:25 -0300

    add: atendente repository

---
+++ Backend/src/DoctorWhen.Domain/Repositories/IAtendenteRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace DoctorWhen.Domain.Repositories;
+public interface IAtendenteRepository : IGeneralRepository
+{
+    Task<Entities.Atendente> GetByIdAsync(long id);
+    Task<Entities.Atendente> GetByEmailAsync(string email);
+}
---
+++ Backend/src/DoctorWhen.Persistence/Repository/AtendenteRepository.cs
@@ -0,0 +1,34 @@
+﻿using DoctorWhen.Domain.Entities;
+using DoctorWhen.Domain.Repositories;
+using DoctorWhen.Persistence.Repository.RepositoryAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWhen.Persistence.Repository;
+public class AtendenteRepository : GeneralRepository, IAtendenteRepository
+{
+    private readonly DoctorWhenContext _context;
+
+    public AtendenteRepository(DoctorWhenContext context) : base(context)
+    {
+        this._context = context;
+    }
+    public async Task<Atendente> GetByEmailAsync(string email)
+    {
+        IQueryable<Atendente> query = _context.Atendentes
+            .Include(a => a.PacientesAtendidos)
+            .Include(a => a.User)
+            .Where(a => a.User.Email == email);
+
+        return await query.SingleOrDefaultAsync();
+    }
+
+    public async Task<Atendente> GetByIdAsync(long id)
+    {
+        IQueryable<Atendente> query = _context.Atendentes
+            .Include(a =>  a.PacientesAtendidos)
+            .Include(a => a.User)
+            .Where(a => a.UserId == id);
+
+        return await query.SingleOrDefaultAsync();
+    }
+}
